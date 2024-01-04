a16bb05424bf64391a8f294503d4036c7ebef943
i'm LOKKI <128024609+lokki-workspace@users.noreply.github.com>
2024-01-04 18:30:12 +0530

    design change commit

--- GUVI-LOKKI-DAY03/day3Task3.js
+++ GUVI-LOKKI-DAY03/day3Task3.js
@@ -14,10 +14,8 @@ request.onload = function()
     // console.log(result);
     var result=JSON.parse(request.response);
     for(let i = 0 ; i< result.length ; i++){
-        console.log(result[i].name);
-        console.log(result[i].region);
-        console.log(result[i].subregion); 
-        console.log(result[i].population);  
+        console.log("NAME:"+ result[i].name,"REGION:"+ result[i].region,
+        "SUB-REGION:"+ result[i].subregion,"POPULATION:"+result[i].population ); 
 }
 }
 
